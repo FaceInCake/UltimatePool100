ed662111d7d67bd7fdfed14e84cb9824902d90e5
Matthew <eppel@uwindsor.ca>
2021-04-05 18:42:30 -0400

    Doc generation cleanup

---
+++ GenerateDocs.bat
@@ -0,0 +1,1 @@
+javadoc -private -sourcepath src;test -encoding UTF-8 -d docs -use -windowtitle "UltimatePool100 Src Documentation" -classpath lib/* appearances behaviours jframes jpanels lights main misc objects
